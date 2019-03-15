a85faca3be304ecdaaa93b903cf931f7a3f1a6f8
Walid Zakaria <walidpiano@yahoo.com>
2019-03-15 02:31:10 +0200

    fix: incorrect syntax

--- contract/views.py
+++ contract/views.py
@@ -8,9 +8,9 @@ from django.core import serializers
 def home(request):
     cities = City.objects.all()
     countries = City.objects.all()
-    serialized_obj = serializers.serialize('json', [ cities, ])
+    #serialized_obj = serializers.serialize('json', [ cities, ])
 
     #result_list = list(cities)
-    return serialized_obj
+    #return serialized_obj
     return render(request, 'contract/index.html',
                   {'cities': cities, 'countries': countries})
