3374ba61d402400d41ac5090d5c17977fb3c6ac8
Walid Zakaria <walidpiano@yahoo.com>
2019-03-17 02:05:26 +0200

    add: login form

--- contract/urls.py
+++ contract/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import home
 
 urlpatterns = [
     url(r'$', home),
+    url(r'accounts/login',
+        LoginView.as_view(template_name="contract/login_form.html"),
+        name="user_login"),
+    url(r'logout',
+        LogoutView.as_view(),
+        name="user_logout")
 ]
--- contract/views.py
+++ contract/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import City, Country
+from .models import City, Country, Hotel
 from django.http import JsonResponse
 from django.core import serializers
 
 
 # Create your views here.
+@login_required
 def home(request):
     cities = City.objects.all()
     countries = City.objects.all()
@@ -12,6 +14,10 @@ def home(request):
 
     #result_list = list(cities)
     #return serialized_obj
+
+    hotels = Hotel.objects.all()
+
     return render(request, 'contract/index.html',
-                  {'cities': cities, 'countries': countries})
+                  {'cities': cities, 'countries': countries, 'hotels': hotels}
+                  )
 
